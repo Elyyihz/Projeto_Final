d89f954c02e9eca818cae8370a285e31d12eda8f
Elynne lima <164261293+Elyyihz@users.noreply.github.com>
2024-06-12 03:10:04 +0000

    Cadastro

--- database.js
+++ database.js
@@ -3,7 +3,6 @@ const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
   db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
-  db.run("INSERT INTO users (username, password) VALUES ('user1', 'password1')");
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
 const db = require('./database');
 const app = express();
 
@@ -11,40 +12,67 @@ app.use(session({
   saveUninitialized: true
 }));
 
-app.get('/', (req, res) => {
-  if (req.session.loggedin) {
-    res.send(`Welcome back, ${req.session.username}! <a href='/logout'>Logout</a>`);
-  } else {
-    res.sendFile(__dirname + '/login.html');
-  }
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Rota para a página de cadastro
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
-app.post('/auth', (req, res) => {
+// Rota para processar o cadastro
+app.post('/register', async (req, res) => {
+  const username = req.body.username;
+  const password = await bcrypt.hash(req.body.password, 10);
+
+  db.run("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err) => {
+    if (err) {
+      return res.send('Erro ao criar usuário');
+    }
+    res.redirect('/login');
+  });
+});
+
+// Rota para a página de login
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Rota para processar o login
+app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (username && password) {
-    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
-      if (row) {
-        req.session.loggedin = true;
-        req.session.username = username;
-        res.redirect('/');
-      } else {
-        res.send('Incorrect Username and/or Password!');
-      }
-      res.end();
-    });
+  db.get('SELECT * FROM users WHERE username = ?', [username], async (err, row) => {
+    if (err || !row) {
+      return res.send('Usuário ou senha incorretos');
+    }
+    const match = await bcrypt.compare(password, row.password);
+    if (match) {
+      req.session.loggedin = true;
+      req.session.username = username;
+      res.redirect('/dashboard');
+    } else {
+      res.send('Usuário ou senha incorretos');
+    }
+  });
+});
+
+// Rota para a página de dashboard (após login)
+app.get('/dashboard', (req, res) => {
+  if (req.session.loggedin) {
+    res.send(`Bem-vindo, ${req.session.username}! <a href='/logout'>Logout</a>`);
   } else {
-    res.send('Please enter Username and Password!');
-    res.end();
+    res.redirect('/login');
   }
 });
 
+// Rota para logout
 app.get('/logout', (req, res) => {
   req.session.destroy();
-  res.redirect('/');
+  res.redirect('/login');
 });
 
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+  console.log('Servidor iniciado em http://localhost:3000');
 });
